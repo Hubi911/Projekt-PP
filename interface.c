14d816ed7d5c37cbf2b88422822510b7cc1c73a9
Hubert Urbanski <adres@e-mail>
2026-01-02 21:01:23 +0100

    Implementacja funkcji pokaz_menu()

--- interface.c
+++ interface.c
@@ -1,3 +1,16 @@
-//
-// Created by Hubert on 02.01.2026.
-//
+#include <stdio.h>
+#include <string.h>
+#include "interface.h"
+
+void pokaz_menu(){
+  printf("\n----ZARZADZAJ FLOTA----\n");
+  printf("1. Lista mechow\n");
+  printf("2. Dodaj nowego mecha\n");
+  printf("3. Usun mecha\n");
+  printf("4. Sortuj po nazwie\n");
+  printf("5. Sortuj po mocy\n");
+  printf("6. Zapisz\n");
+  printf("7. Wyjdz\n");
+  printf("Twoj wybor: ");
+ }
+ void wypisz(List *b){}
