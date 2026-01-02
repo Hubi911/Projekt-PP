0f96dca38f33726ddf45def4e39b6c19d43ba39f
Hubert Urbanski <adres@e-mail>
2026-01-02 20:49:51 +0100

    Deklaracja funkcji do działania na danych

--- logic.h
+++ logic.h
@@ -1,8 +1,14 @@
-//
-// Created by Hubert on 02.01.2026.
-//
-
 #ifndef LOGIC_H
 #define LOGIC_H
+#include "struct.h"
 
-#endif //LOGIC_H
+void twozenie_listy(List *b);
+void zwolnij_pamiec(List *b);
+int dodaj(List *b, Mech nowy);
+int usun(List *b, char *model);
+void sort_nazwa(List *b);
+void sort_moc(List *b);
+void zapisz(List *b, char *nazwa_pliku);
+void wczytaj(List *b, char *nazwa_pliku);
+
+#endif
