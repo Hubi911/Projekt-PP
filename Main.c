b4e83d70f6d0c08134bcc0556ad2a00a22e32330
Hubert Urbanski <adres@e-mail>
2026-01-03 15:41:29 +0100

    STworzenie pliku tekstowego z danymi i uzupełnienie pliku Main do testów

--- Main.c
+++ Main.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "struct.h"
+#include "logic.h"
+#include "interface.h"
 
+int main(int argc, char *argv[]){
+  List Mechy_lista;
+  inicjalizacja_listy(&Mechy_lista);
+
+  wczytaj(&Mechy_lista, "projekt_mechy.txt");
+  wypisz(&Mechy_lista);
+  return 0;
+  }
