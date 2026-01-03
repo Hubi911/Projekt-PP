e75a52e3c90d1b18cf23ce1c71ba2f5716cd679b
Hubert Urbanski <adres@e-mail>
2026-01-03 12:12:46 +0100

    Implementacja Funkcji do wczytywania danych z pliku

--- logic.c
+++ logic.c
@@ -1,3 +1,31 @@
-//
-// Created by Hubert on 02.01.2026.
-//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "logic.h"
+
+void twozenie_listy(List *b){
+  b->head = NULL;
+  }
+void zwolnij_pamiec(List *b){
+  }
+int dodaj(List *b, Mech nowy){
+  }
+int usun(List *b, char *model){
+  }
+void sort_nazwa(List *b){
+  }
+void sort_moc(List *b){
+  }
+void zapisz(List *b, char *nazwa_pliku){
+  }
+void wczytaj(List *b, char *nazwa_pliku){
+  FILE *plik = fopen(nazwa_pliku,"r");
+  if (plik == NULL){
+    return;
+  }
+  Mech robo;
+  while(fscanf(plik," %100s %100s %d %100s %100s",robo.model, robo.klasa, &robo.moc, robo.pilot, robo.stan) == 5){
+    dodaj(b, robo);
+  }
+  fclose(plik);
+}
